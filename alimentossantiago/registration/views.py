72e85694b1e9e278bf9073fabc09d39f7d775a37
Juan Manuel <ju.ponce@duocuc.cl>
2022-06-13 17:39:48 -0400

    registro completo, sin validaciones

--- alimentossantiago/registration/views.py
+++ alimentossantiago/registration/views.py
@@ -1,12 +1,23 @@
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 # Create your views here.
 
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     template_name = 'registration/signup.html'
+
+#     def get_success_url(self):
+#         return reverse_lazy('login') + '?register'
+
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpFormWithEmail
     template_name = 'registration/signup.html'
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    # def get_form(self, form_class=None):
+    #     form = super(SignUpView, self).get_form()
