142b93057c9ef951f39d44c33dc6ff9d54d184c3
juponce <102298230+juponce@users.noreply.github.com>
2023-01-02 15:14:20 -0300

    Update settings.py

--- alimentossantiago/alimentossantiago/settings.py
+++ alimentossantiago/alimentossantiago/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'alimentos_santiago',
         'USER': 'root',
-        'PASSWORD': 'bkfw7SrSndx8',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
